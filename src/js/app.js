52b5b16783e22ecb432adaef68392eefda077d70
Axel Lanuza <rojasf@ae.com>
2024-11-20 13:48:24 -0600

    2. Finihs this project just with css

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,12 @@
-import { DatePicker } from "./test-module";
+import { TooltipUI } from "./test-module";
 import "../css/app.css";
 import "../scss/style.scss";
 
 /********** Paste your code here! ************/
 
-window.onload = () => {
-  console.log("Paste your code here!");
-  DatePicker.init();
-};
+// window.onload = () => {
+//   console.log("Paste your code here!");
+//   TooltipUI.init();
+// };
+
+document.addEventListener("DOMContentLoaded", TooltipUI.init);
--- src/js/test-module.js
+++ src/js/test-module.js
@@ -1,16 +1,72 @@
-export const AppBae = (() => {
-  console.log("DatePicker");
-  /*
-  *   DOM refs
-  *
-  * */
+export const TooltipUI = (() => {
+  console.log("TooltipUI");
+
+  const $tooltip = document.querySelector(".tooltip");
+  const $navElements = document.querySelectorAll(".menu ul li");
 
   const init = () => {
     console.log("Init app");
     eventHandlers();
   };
 
+  const tooltipTemplate = `
+    <div class="tooltip">
+      <p></p>
+    </div>
+  `;
+
   const eventHandlers = () => {
+    $navElements.forEach((el) => {
+      el.addEventListener("mouseover", (ev) => {
+        const target = ev.target;
+        showTooltip(target, "bottom"); // Aquí puedes cambiar la posición a 'top', 'bottom', 'right' o 'left'
+      });
+      el.addEventListener("mouseout", () => {
+        hideTooltip();
+      });
+    });
+  };
+  // TODO: implement properly this function
+  const showTooltip = (target, position) => {
+    //target.appendChild(tooltipTemplate);
+    // const tooltipText = target.dataset.tooltip;
+    // const tooltipPosition = target.dataset.position || position;
+    // $tooltip.querySelector("p").textContent = tooltipText;
+    // $tooltip.style.display = "block";
+    // // Obtener las dimensiones del target y del tooltip
+    // const rect = target.getBoundingClientRect();
+    // const tooltipRect = $tooltip.getBoundingClientRect();
+    // console.log({ rect, tooltipRect });
+    // let top, left;
+    // // switch (tooltipPosition) {
+    // //   case "top":
+    // //     top = rect.top - tooltipRect.height - 10;
+    // //     left = rect.left + rect.width / 2 - tooltipRect.width / 2;
+    // //     break;
+    // //   case "bottom":
+    // //     top = rect.bottom + 10;
+    // //     left = rect.left + rect.width / 2 - tooltipRect.width / 2;
+    // //     break;
+    // //   case "right":
+    // //     top = rect.top + rect.height / 2 - tooltipRect.height / 2;
+    // //     left = rect.right + 10;
+    // //     break;
+    // //   case "left":
+    // //     top = rect.top + rect.height / 2 - tooltipRect.height / 2;
+    // //     left = rect.left - tooltipRect.width - 10;
+    // //     break;
+    // //   default:
+    // //     top = rect.bottom + 10;
+    // //     left = rect.left + rect.width / 2 - tooltipRect.width / 2;
+    // // }
+    // $tooltip.style.top = `${top}px`;
+    // $tooltip.style.left = `${left}px`;
+    // $tooltip.classList.add(tooltipPosition);
+  };
+
+  const hideTooltip = () => {
+    $tooltip.style.display = "none";
+    $tooltip.classList.remove("top", "bottom", "right", "left");
   };
 
   return {
